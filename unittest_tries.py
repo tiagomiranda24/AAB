5e6808f2a28d4a99bd5ac482b0244f8a7d62678e
ChristianNeitzel <pg52526@uminho.pt>
2024-06-10 20:43:11 +0100

    renomeado 'trie' para 'tries'

--- unittest_tries.py
+++ unittest_tries.py
@@ -1,55 +1,55 @@
 import unittest
 from unittest.mock import patch
-from trie import Trie
+from tries import Tries
 
-class TestTrieMethods(unittest.TestCase):
+class TestTriesMethods(unittest.TestCase):
 
     def setUp(self):
-        self.trie = Trie()
+        self.tries = Tries()
 
     def test_insert(self):
-        self.trie.insert('apple')
-        self.assertTrue(self.trie.search('apple'))
+        self.tries.insert('apple')
+        self.assertTrue(self.tries.search('apple'))
 
     def test_insert_fromList(self):
         word_list = ['apple', 'banana', 'orange']
-        self.trie.insert_fromList(word_list)
+        self.tries.insert_fromList(word_list)
         for word in word_list:
-            self.assertTrue(self.trie.search(word))
+            self.assertTrue(self.tries.search(word))
 
     def test_insert_fromInput(self):
         input_values = ['3', 'apple', 'orange', 'banana']
         
         with patch('builtins.input', side_effect=input_values):
-            self.trie.insert_fromInput()
+            self.tries.insert_fromInput()
             
-        self.assertTrue(self.trie.search('apple'))
-        self.assertTrue(self.trie.search('orange'))
-        self.assertTrue(self.trie.search('banana'))
+        self.assertTrue(self.tries.search('apple'))
+        self.assertTrue(self.tries.search('orange'))
+        self.assertTrue(self.tries.search('banana'))
 
     def test_delete(self):
-        self.trie.insert('apple')
-        self.trie.delete('apple')
-        self.assertFalse(self.trie.search('apple'))
+        self.tries.insert('apple')
+        self.tries.delete('apple')
+        self.assertFalse(self.tries.search('apple'))
 
     def test_delete_fromList(self):
         word_list = ['apple', 'banana', 'orange']
-        self.trie.insert_fromList(word_list)
-        self.trie.delete_fromList(['apple', 'banana'])
-        self.assertFalse(self.trie.search('apple'))
-        self.assertFalse(self.trie.search('banana'))
-        self.assertTrue(self.trie.search('orange'))
+        self.tries.insert_fromList(word_list)
+        self.tries.delete_fromList(['apple', 'banana'])
+        self.assertFalse(self.tries.search('apple'))
+        self.assertFalse(self.tries.search('banana'))
+        self.assertTrue(self.tries.search('orange'))
 
     def test_search(self):
-        self.trie.insert('apple')
-        self.assertTrue(self.trie.search('apple'))
-        self.assertFalse(self.trie.search('banana'))
+        self.tries.insert('apple')
+        self.assertTrue(self.tries.search('apple'))
+        self.assertFalse(self.tries.search('banana'))
 
     def test_search_fromList(self):
         words_to_insert = ['apple', 'pineapple', 'clementine', 'watermelon']
-        self.trie.insert_fromList(words_to_insert)
+        self.tries.insert_fromList(words_to_insert)
         words_to_search = ['apple', 'banana', 'orange', 'pineapple', 'strawberry', 'clementine']
-        search_result = self.trie.search_fromList(words_to_search)
+        search_result = self.tries.search_fromList(words_to_search)
 
         expected_result = {
             'apple': True,
@@ -63,7 +63,7 @@ class TestTrieMethods(unittest.TestCase):
 
     def test_search_fromInput(self):
         words_to_insert = ['apple', 'pineapple', 'clementine', 'watermelon']
-        self.trie.insert_fromList(words_to_insert)
+        self.tries.insert_fromList(words_to_insert)
 
         input_values = ['apple', 'banana', 'orange', 'pineapple', 'strawberry', 'clementine', '']
         input_iter = iter(input_values)
@@ -72,21 +72,21 @@ class TestTrieMethods(unittest.TestCase):
             return next(input_iter)
 
         with patch('builtins.input', side_effect=mock_input):
-            found_words, not_found_words = self.trie.search_fromInput()
+            found_words, not_found_words = self.tries.search_fromInput()
 
         self.assertEqual(found_words, ['apple', 'pineapple', 'clementine'])
         self.assertEqual(not_found_words, ['banana', 'orange', 'strawberry'])
 
     def test_startsWith(self):
-        self.trie.insert('apple')
-        self.trie.insert('banana')
-        self.assertTrue(self.trie.startsWith('app'))
-        self.assertFalse(self.trie.startsWith('orn'))
+        self.tries.insert('apple')
+        self.tries.insert('banana')
+        self.assertTrue(self.tries.startsWith('app'))
+        self.assertFalse(self.tries.startsWith('orn'))
 
     def test_getAllWords(self):
         word_list = ['apple', 'banana', 'orange']
-        self.trie.insert_fromList(word_list)
-        retrieved_words = self.trie.getAllWords()
+        self.tries.insert_fromList(word_list)
+        retrieved_words = self.tries.getAllWords()
         for word in word_list:
             self.assertIn(word, retrieved_words)
 
